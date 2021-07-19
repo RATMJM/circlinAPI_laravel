c3111dd743b76951ea618806c59168df4a0c99d1
lastedchan <lastedchan@gamil.com>
2021-07-19 11:12:03 +0900

    test

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 1;
+Route::get('/', function (Request $request) {
+    return $request->server('REMOTE_ADDR');
 });
