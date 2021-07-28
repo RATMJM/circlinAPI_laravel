cf63e344322c6c747a724742ff6701c74db3ed0e
lastedchan <451575@naver.com>
2021-07-28 16:48:53 +0900

    return type 지정, 로그인 추가

--- app/Http/Controllers/v1/AuthController.php
+++ app/Http/Controllers/v1/AuthController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
-use App\Models\UserStat;
-use Firebase\JWT\JWT;
-use \Illuminate\Support\Facades\Hash;
-use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\UserStat;
+use Exception;
+use Firebase\JWT\JWT;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function exists_email($email)
+    public function exists_email($email): JsonResponse
     {
         try {
             $exists = User::where('email', $email)->exists();
@@ -22,14 +25,14 @@ class AuthController extends Controller
                     'exists' => $exists,
                 ]
             ]);
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             return response()->json([
                 'success' => false,
             ]);
         }
     }
 
-    public function exists_nickname($nickname)
+    public function exists_nickname($nickname): JsonResponse
     {
         try {
             $exists = User::where('nickname', $nickname)->exists();
@@ -40,40 +43,55 @@ class AuthController extends Controller
                     'exists' => $exists,
                 ]
             ]);
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             return response()->json([
                 'success' => false,
             ]);
         }
     }
 
-    public function login(Request $request)
+    public function login(Request $request): JsonResponse
     {
-        $email = $request->get('email');
-        $password = $request->get('password');
+        try {
+            $email = $request->get('email');
+            $password = $request->get('password');
 
-        $user = User::where(['email' => $email])->first();
-        $user_stat = UserStat::where('user_id', $user->id)->first();
-        if (isset($user) && ($user->password === '' || Hash::check($password, $user->password))) {
-            return [
-                'success' => true,
-                'data' => [
-                    'token' => JWT::encode([
-                        'iss' => 'https://www.circlin.co.kr',
-                        'aud' => 'https://www.circlin.co.kr',
-                        'iat' => 1356999524,
-                        'nbf' => 1357000000,
-                    ], env('JWT_SECRET')),
-                    'user' => [
-
+            $user = User::where(['email' => $email])->first();
+            if (isset($user) && ($user->password === '' || Hash::check($password, $user->password))) {
+                $user_stat = $user->stat;
+                return response()->json([
+                    'success' => true,
+                    'data' => [
+                        'token' => JWT::encode([
+                            'iss' => 'https://www.circlin.co.kr',
+                            'aud' => 'https://www.circlin.co.kr',
+                            'iat' => 1356999524,
+                            'nbf' => 1357000000,
+                            'uid' => $user->id,
+                        ], env('JWT_SECRET')),
+                        'user' => [
+                            'name' => $user->name,
+                            'nickname' => $user->nickname,
+                            'phone' => $user->phone,
+                            'point' => $user->point,
+                            'birth' => $user->stat->birth,
+                            'gender' => $user->stat->gender,
+                            'height' => $user->stat->height,
+                            'weight' => $user->stat->weight,
+                            'bmi' => $user->stat->bmi,
+                        ],
                     ],
-                ],
-            ];
-        } else {
-            return [
-                'success' => true,
-                'data' => ['token' => null, 'user' => null],
-            ];
+                ]);
+            } else {
+                return response()->json([
+                    'success' => true,
+                    'data' => ['token' => null, 'user' => null],
+                ]);
+            }
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+            ]);
         }
     }
 }
