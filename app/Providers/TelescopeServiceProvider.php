7911753be099e3557a73554778e1b2db4b97bd5a
lastedchan <451575@naver.com>
2021-07-29 14:58:40 +0900

    telescope 복구

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use App\Http\Middleware\BlockIp;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Telescope\IncomingEntry;
 use Laravel\Telescope\Telescope;
@@ -23,11 +22,6 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
 
         Telescope::filter(function (IncomingEntry $entry) {
             if ($this->app->environment('local')) {
-                return $entry->isReportableException() ||
-                    $entry->isFailedRequest() ||
-                    $entry->isFailedJob() ||
-                    $entry->isScheduledTask() ||
-                    $entry->hasMonitoredTag();
                 return true;
             }
 
@@ -69,10 +63,9 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewTelescope', function ($user) {
-            return in_array($_SERVER['REMOTE_ADDR'], ALLOW_IP);
-            /*return in_array($user->email, [
+            return in_array($user->email, [
                 //
-            ]);*/
+            ]);
         });
     }
 }
