c331bdab85f5d8dbfcd8da949a163d75bb330b11
lastedchan <451575@naver.com>
2021-08-02 14:59:44 +0900

    profile_image method patch 로 수정

--- routes/api.php
+++ routes/api.php
@@ -42,7 +42,7 @@ Route::group(['prefix' => 'auth'], function () {
 Route::group(['prefix' => 'user'], function () {
     Route::get('/', 'UserController@user');
     Route::patch('/profile', 'UserController@update_profile');
-    Route::post('/profile_image', 'UserController@change_profile_image');
+    Route::patch('/profile_image', 'UserController@change_profile_image');
     Route::post('/favorite_category', 'UserController@add_favorite_category');
     Route::delete('/favorite_category', 'UserController@remove_favorite_category');
     Route::post('/follow', 'UserController@follow');
