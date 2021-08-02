8f4e391dc35784253de35deb2b04c1f455e8bc1e
lastedchan <451575@naver.com>
2021-08-02 14:34:34 +0900

    팔로우할 유저 추천 배열 개선

--- app/Http/Controllers/v1/BaseController.php
+++ app/Http/Controllers/v1/BaseController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Area;
 use App\Models\MissionCategory;
 use App\Models\User;
+use App\Models\UserStat;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -27,11 +28,20 @@ class BaseController extends Controller
 
     public function suggest_user(Request $request): array
     {
+        $user_id = token()->uid;
+
         $limit = max(min($request->get('limit', 50), 50), 1);
 
-        return User::select(['id', 'nickname', 'profile_image'])
-            ->addSelect(['area' => Area::selectRaw('CONCAT_WS(\' \', name_lg, name_md, name_sm)')
-                ->whereColumn('users.area_code', 'areas.ctg_sm')->limit(1)])
-            ->inRandomOrder()->take($limit)->get()->toArray();
+        return success([
+            'result' => true,
+            'users' => User::select(['users.id', 'users.nickname', 'users.profile_image', 'user_stats.gender',
+                DB::raw("CONCAT_WS(' ', areas.name_lg, areas.name_md, areas.name_sm) as area"),
+                DB::raw("COUNT(distinct follows.id) as follower")])
+                ->leftJoin('user_stats', 'user_stats.user_id', 'users.id')
+                ->leftJoin('areas', 'areas.ctg_sm', 'users.area_code')
+                ->leftJoin('follows', 'follows.target_id', 'users.id')
+                ->groupBy(['users.id', 'user_stats.id', 'areas.id'])
+                ->inRandomOrder()->take($limit)->get(),
+        ]);
     }
 }
