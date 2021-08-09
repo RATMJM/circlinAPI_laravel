48552fd54b5dbb228e12de22106698ccae928661
lastedchan <451575@naver.com>
2021-08-09 20:25:28 +0900

    카테고리 미션목록 api 및 town api 반환데이터 개선

--- app/Http/Controllers/v1/MissionCategoryController.php
+++ app/Http/Controllers/v1/MissionCategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Follow;
 use App\Models\Mission;
 use App\Models\MissionCategory;
 use App\Models\User;
@@ -84,29 +85,40 @@ class MissionCategoryController extends Controller
         if ($id) {
             $data = Mission::where('mission_category_id', $id)
                 ->join('users as o', 'o.id', 'missions.user_id') // 미션 제작자
+                ->join('user_stats as os', 'os.user_id', 'o.id') // 미션 제작자
+                ->leftJoin('follows as of', 'of.target_id', 'o.id') // 미션 제작자 팔로워
+                ->leftJoin('areas as oa', 'oa.ctg_sm', 'o.area_code')
                 ->leftJoin('user_missions as um', function ($query) {
                     $query->on('um.mission_id', 'missions.id')->whereNull('um.deleted_at');
                 })
                 ->leftJoin('mission_comments as mc', 'mc.mission_id', 'missions.id')
                 ->select([
                     'missions.id', 'missions.title', 'missions.description',
-                    DB::raw("CONCAT(COALESCE(o.id, ''), '|', COALESCE(o.profile_image, '')) as owner"),
+                    'o.id as user_id', 'o.nickname', 'o.profile_image', 'os.gender',
+                    DB::raw("IF(name_lg=name_md, CONCAT_WS(' ', name_md, name_sm), CONCAT_WS(' ', name_lg, name_md, name_sm)) as area"),
+                    DB::raw("COUNT(distinct of.user_id) as followers"),
+                    'is_following' => Follow::selectRaw(1)->whereColumn('follows.target_id', 'o.id')
+                        ->where('follows.user_id', $user_id)->limit(1),
                     'is_bookmark' => UserMission::selectRaw('COUNT(1)>0')->where('user_missions.user_id', $user_id)
                         ->whereColumn('user_missions.mission_id', 'missions.id')->limit(1),
-                    'user1' => UserMission::selectRaw("CONCAT(COALESCE(u.id, ''), '|', COALESCE(u.profile_image, ''))")
+                    'user1' => UserMission::selectRaw("CONCAT_WS('|', COALESCE(u.id, ''), COALESCE(u.nickname, ''),
+                        COALESCE(u.profile_image, ''), COALESCE(us.gender, ''))")
                         ->whereColumn('user_missions.mission_id', 'missions.id')
                         ->join('users as u', 'u.id', 'user_missions.user_id')
+                        ->leftJoin('user_stats as us', 'us.user_id', 'u.id')
                         ->leftJoin('follows as f', 'f.target_id', 'user_missions.user_id')
-                        ->groupBy('u.id')->orderBy(DB::raw('COUNT(f.id)'), 'desc')->limit(1),
-                    'user2' => UserMission::selectRaw("CONCAT(COALESCE(u.id, ''), '|', COALESCE(u.profile_image, ''))")
+                        ->groupBy('u.id', 'us.id')->orderBy(DB::raw('COUNT(f.id)'), 'desc')->limit(1),
+                    'user2' => UserMission::selectRaw("CONCAT_WS('|', COALESCE(u.id, ''), COALESCE(u.nickname, ''),
+                        COALESCE(u.profile_image, ''), COALESCE(us.gender, ''))")
                         ->whereColumn('user_missions.mission_id', 'missions.id')
                         ->join('users as u', 'u.id', 'user_missions.user_id')
+                        ->leftJoin('user_stats as us', 'us.user_id', 'u.id')
                         ->leftJoin('follows as f', 'f.target_id', 'user_missions.user_id')
-                        ->groupBy('u.id')->orderBy(DB::raw('COUNT(f.id)'), 'desc')->skip(1)->limit(1),
+                        ->groupBy('u.id', 'us.id')->orderBy(DB::raw('COUNT(f.id)'), 'desc')->skip(1)->limit(1),
                     DB::raw('COUNT(distinct um.id) as bookmarks'),
                     DB::raw('COUNT(distinct mc.id) as comments'),
                 ])
-                ->groupBy('missions.id', 'o.id');
+                ->groupBy('missions.id', 'o.id', 'os.id', 'oa.id');
 
             if ($sort === 'popular') {
                 $data->orderBy('bookmarks', 'desc')->orderBy('missions.id', 'desc');
@@ -119,14 +131,25 @@ class MissionCategoryController extends Controller
             $data = $data->skip($page * $limit)->take($limit)->get();
 
             foreach($data as $i => $item) {
-                $tmp = explode('|', $item['owner'] ?? '|');
-                $data[$i]['owner'] = ['user_id' => $tmp[0], 'profile_image' => $tmp[1]];
-                $tmp1 = explode('|', $item['user1'] ?? '|');
-                $tmp2 = explode('|', $item['user2'] ?? '|');
-                $data[$i]['users'] = [
-                    ['user_id' => $tmp1[0], 'profile_image' => $tmp1[1]],['user_id' => $tmp2[0], 'profile_image' => $tmp2[1]]
+                $data[$i]['is_bookmark'] = (bool)$item->is_bookmark;
+                $data[$i]['owner'] = [
+                    'user_id' => $item->user_id,
+                    'nickname' => $item->nickname,
+                    'profile_image' => $item->profile_image ?? '',
+                    'gender' => $item->gender,
+                    'area' => $item->area,
+                    'followers' => $item->followers,
+                    'is_following' => (bool)$item->is_following,
                 ];
-                unset($data[$i]['user1'], $data[$i]['user2']);
+                unset($data[$i]->user_id, $data[$i]->nickname, $data[$i]->profile_image, $data[$i]->gender,
+                    $data[$i]->area, $data[$i]->followers, $data[$i]->is_following);
+                $tmp1 = explode('|', $item['user1'] ?? '|||');
+                $tmp2 = explode('|', $item['user2'] ?? '|||');
+                $data[$i]['users'] = [
+                    ['user_id' => $tmp1[0], 'nickname' => $tmp1[1], 'profile_image' => $tmp1[2], 'gender' => $tmp1[3]],
+                    ['user_id' => $tmp2[0], 'nickname' => $tmp2[1], 'profile_image' => $tmp2[2], 'gender' => $tmp2[3]],
+                ];
+                unset($data[$i]->user1, $data[$i]->user2);
             }
 
             return success([
