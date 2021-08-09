eacd288a940ed4c884a6a526d310dd03c2525e2d
lastedchan <451575@naver.com>
2021-08-09 19:20:30 +0900

    배너 더미데이터 개선

--- app/Http/Controllers/v1/BannerController.php
+++ app/Http/Controllers/v1/BannerController.php
@@ -11,27 +11,27 @@ class BannerController extends Controller
     {
         return [
             [
-                'banner_image' => 'https://via.placeholder.com/1500x750',
+                'banner_image' => 'https://cyld20183.speedgabia.com/Image/BANNER/KRKO_3UP_STATIC_1000x625_BAN_WATCH_NA_STA_BNOW_NA.jpg',
                 'link_type' => 'url',
                 'link_url' => 'https://via.placeholder.com/1500x750',
             ],
             [
-                'banner_image' => 'https://via.placeholder.com/1500x750',
+                'banner_image' => 'https://cyld20183.speedgabia.com/Image/BANNER/20210809-challenge-habit-mymealprotein3-app-banner-ad-h.png',
                 'link_type' => 'url',
                 'link_url' => 'https://via.placeholder.com/1500x750',
             ],
             [
-                'banner_image' => 'https://via.placeholder.com/1500x750',
+                'banner_image' => 'https://cyld20183.speedgabia.com/Image/BANNER/20210809-challenge-habit-arginine3-app-banner-ad-h.png',
                 'link_type' => 'url',
                 'link_url' => 'https://via.placeholder.com/1500x750',
             ],
             [
-                'banner_image' => 'https://via.placeholder.com/1500x750',
+                'banner_image' => 'https://cyld20183.speedgabia.com/Image/BANNER/20210802-challenge-run-815virtual-app-banner-ad-h.png',
                 'link_type' => 'url',
                 'link_url' => 'https://via.placeholder.com/1500x750',
             ],
             [
-                'banner_image' => 'https://via.placeholder.com/1500x750',
+                'banner_image' => 'https://cyld20183.speedgabia.com/Image/BANNER/20210722-challenge-habbit-Growjee-app-banner-ad-h.png',
                 'link_type' => 'url',
                 'link_url' => 'https://via.placeholder.com/1500x750',
             ],
