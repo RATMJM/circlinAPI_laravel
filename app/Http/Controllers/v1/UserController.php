d99a619d12fda5e8043142e243626d98ee17bf99
lastedchan <451575@naver.com>
2021-08-10 17:27:26 +0900

    image_url -> image 컬럼명 변경된부분 수정

--- app/Http/Controllers/v1/UserController.php
+++ app/Http/Controllers/v1/UserController.php
@@ -420,7 +420,7 @@ class UserController extends Controller
                 DB::raw("COUNT(distinct fpr.id) > 0 as has_product"), // 상품 있는지
                 DB::raw("COUNT(distinct fpl.id) > 0 as has_place"), // 위치 있는지
                 'image_type' => FeedImage::select('type')->whereColumn('feed_images.feed_id', 'feeds.id')->orderBy('id')->limit(1),
-                'image' => FeedImage::select('image_url')->whereColumn('feed_images.feed_id', 'feeds.id')->orderBy('id')->limit(1),
+                'image' => FeedImage::select('image')->whereColumn('feed_images.feed_id', 'feeds.id')->orderBy('id')->limit(1),
                 DB::raw('COUNT(distinct fm.id) as missions'),
                 'mission_id' => FeedMission::select('mission_id')->whereColumn('feed_missions.feed_id', 'feeds.id')
                     ->orderBy('id')->limit(1),
@@ -474,7 +474,7 @@ class UserController extends Controller
                 DB::raw("COUNT(distinct fpr.id) > 0 as has_product"), // 상품 있는지
                 DB::raw("COUNT(distinct fpl.id) > 0 as has_place"), // 위치 있는지
                 'image_type' => FeedImage::select('type')->whereColumn('feed_images.feed_id', 'f.id')->orderBy('id')->limit(1),
-                'image' => FeedImage::select('image_url')->whereColumn('feed_images.feed_id', 'f.id')
+                'image' => FeedImage::select('image')->whereColumn('feed_images.feed_id', 'f.id')
                     ->orderBy('id')->limit(1),
                 DB::raw('COUNT(distinct fm.id) as missions'),
                 'mission_id' => FeedMission::select('mission_id')->whereColumn('feed_missions.feed_id', 'f.id')
