89585caa2e9f0d4b42eea4fa3b877265bc9ae153
lastedchan <451575@naver.com>
2021-08-11 16:39:45 +0900

    GET /suggest_user 랜덤정렬에서 id 정렬로 임시 수정

--- app/Http/Controllers/v1/BaseController.php
+++ app/Http/Controllers/v1/BaseController.php
@@ -7,6 +7,7 @@ use App\Models\Area;
 use App\Models\Follow;
 use App\Models\MissionCategory;
 use App\Models\User;
+use App\Models\UserStat;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -34,25 +35,30 @@ class BaseController extends Controller
 
         $limit = max(min($request->get('limit', 50), 50), 1);
 
+        DB::enableQueryLog();
+
+        $users = User::where('users.id', '!=', $user_id)
+            ->whereNotNull('users.nickname')
+            ->whereDoesntHave('followers', function ($query) use ($user_id) {
+                $query->where('user_id', $user_id);
+            })
+            ->select([
+                'users.id', 'users.nickname', 'users.profile_image',
+                'gender' => UserStat::select('gender')->whereColumn('user_stats.user_id', 'users.id')->limit(1),
+                'area' => Area::selectRaw("IF(name_lg=name_md, CONCAT_WS(' ', name_md, name_sm), CONCAT_WS(' ', name_lg, name_md, name_sm))")
+                    ->whereColumn('areas.ctg_sm', 'users.area_code')->limit(1),
+                'follower' => Follow::selectRaw("COUNT(1)")->whereColumn('follows.target_id', 'users.id'),
+                'is_following' => Follow::selectRaw("COUNT(1) > 0")->whereColumn('follows.target_id', 'users.id')
+                    ->where('follows.user_id', $user_id),
+            ])
+            ->orderBy('id')
+            ->take($limit)->get();
+
+        dd(DB::getQueryLog());
+
         return success([
             'result' => true,
-            'users' => User::where('users.id', '!=', $user_id)
-                ->whereNotNull('users.nickname')
-                ->whereDoesntHave('followers', function ($query) use ($user_id) {
-                    $query->where('user_id', $user_id);
-                })
-                ->leftJoin('user_stats', 'user_stats.user_id', 'users.id')
-                ->leftJoin('areas', 'areas.ctg_sm', 'users.area_code')
-                ->leftJoin('follows', 'follows.target_id', 'users.id')
-                ->select([
-                    'users.id', 'users.nickname', 'users.profile_image', 'user_stats.gender',
-                    DB::raw("IF(name_lg=name_md, CONCAT_WS(' ', name_md, name_sm), CONCAT_WS(' ', name_lg, name_md, name_sm)) as area"),
-                    DB::raw("COUNT(distinct follows.id) as follower"),
-                    'is_following' => Follow::selectRaw("COUNT(1) > 0")->whereColumn('follows.target_id', 'users.id')
-                        ->where('follows.user_id', $user_id),
-                ])
-                ->groupBy(['users.id', 'user_stats.id', 'areas.id'])
-                ->inRandomOrder()->take($limit)->get(),
+            'users' => $users,
         ]);
     }
 }
