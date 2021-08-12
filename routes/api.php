13393328fb1c419fc54f4ad1d29f91daebd62c5f
lastedchan <451575@naver.com>
2021-08-12 12:20:54 +0900

    PATCH /user/profile/image method POST 로 수정

--- routes/api.php
+++ routes/api.php
@@ -44,7 +44,7 @@ Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
 Route::group(['prefix' => 'user', 'as' => 'user.'], function () {
     Route::get('/', [v1\UserController::class, 'index'])->name('index');
     Route::patch('/profile', [v1\UserController::class, 'update'])->name('profile.update');
-    Route::patch('/profile/image', [v1\UserController::class, 'change_profile_image'])->name('profile.image.update');
+    Route::post('/profile/image', [v1\UserController::class, 'change_profile_image'])->name('profile.image.update');
     Route::delete('/profile/image', [v1\UserController::class, 'remove_profile_image'])->name('profile.image.delete');
     Route::resource('favorite_category',v1\UserFavoriteCategoryController::class);
     Route::get('/follower', [v1\UserController::class, 'follower'])->name('follower');
