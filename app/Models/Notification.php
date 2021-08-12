912ad5aa4d0eae9af2488df0eec57f4756ea7061
lastedchan <451575@naver.com>
2021-08-12 11:24:15 +0900

    GET /notification 추가

---
+++ app/Models/Notification.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function feed()
+    {
+        return $this->belongsTo('App\Models\Feed');
+    }
+
+    public function feed_comment()
+    {
+        return $this->belongsTo('App\Models\FeedComment');
+    }
+
+    public function mission()
+    {
+        return $this->belongsTo('App\Models\Mission');
+    }
+
+    public function mission_commenet()
+    {
+        return $this->belongsTo('App\Models\MissionComment');
+    }
+}
