e5e576f4eeae28576c6c4a2a348b9177c5497da3
lastedchan <451575@naver.com>
2021-08-17 22:56:59 +0900

    chat index 수정 내가 보낸 메시지도 읽지 않은 메시지로 처리하는 문제 수정

--- app/Http/Controllers/v1/ChatController.php
+++ app/Http/Controllers/v1/ChatController.php
@@ -233,7 +233,8 @@ class ChatController extends Controller
                 'latest_at' => ChatMessage::select('created_at')->whereColumn('chat_room_id', 'chat_users.chat_room_id')
                     ->orderBy('id', 'desc')->limit(1),
                 'unread_total' => ChatMessage::selectRaw("COUNT(1)")->whereColumn('chat_room_id', 'chat_users.chat_room_id')
-                    ->whereColumn('chat_messages.id', '>', DB::raw("COALESCE(read_message_id, 0)")),
+                    ->whereColumn('chat_messages.id', '>', DB::raw("COALESCE(read_message_id, 0)"))
+                    ->where('user_id', '!=', $user_id),
             ])
             ->orderBy('latest_at', 'desc')
             ->get();
