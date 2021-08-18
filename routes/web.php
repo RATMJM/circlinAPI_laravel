3d89a130f63d59d21b3c4bf867c781831fc9d965
lastedchan <451575@naver.com>
2021-08-18 17:10:57 +0900

    web.php 테스트 함수 제거

--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function (Request $request) {
     return $request->ip();
 });
-
-Route::get('/user/profile/image', function () {
-    echo "<form method='post' action='/v1/user/profile/image' enctype='multipart/form-data'>
-    <input type='file' name='file' id='file'>
-    <button type='submit'>업로드</button>
-</form>";
-});
-
-Route::get('/feed', function () {
-    echo "<form method='post' action='/v1/feed' enctype='multipart/form-data'>
-    <textarea name='content' cols='30' rows='10'></textarea><br>
-    <input type='checkbox' name='missions[]' checked value='1'>
-    <input type='checkbox' name='missions[]' checked value='20'>
-    <input type='checkbox' name='missions[]' checked value='40'>
-    <br>
-    <input type='file' name='files[]' multiple><br>
-    <button type='submit'>업로드</button>
-</form>";
-});
