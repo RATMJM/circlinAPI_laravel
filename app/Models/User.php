322151876ed56f2dca17fa8665b00ac2624c3323
lastedchan <451575@naver.com>
2021-08-20 21:58:37 +0900

    users 모델 guarded 초기화

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable, SoftDeletes;
 
-    protected $guarded = ['point'];
+    protected $guarded = [];
 
     protected $hidden = [
         'password',
