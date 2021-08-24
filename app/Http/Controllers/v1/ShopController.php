eda0e665c35989b724ba29b5138f06ded4236826
JMMMM3 <jm@circlin.co.kr>
2021-08-24 20:32:18 +0900

    shop/cart 수정

--- app/Http/Controllers/v1/ShopController.php
+++ app/Http/Controllers/v1/ShopController.php
@@ -440,20 +440,20 @@ class ShopController extends Controller
                 
                 try {
              
-                    foreach ($items as $key => $value){  
+                    foreach ($options as $key => $value){  
                         
                         $option = DB::insert('INSERT into cart_options(created_at, updated_at, cart_id, product_option_id, price)
-                                            VALUES(?, ?, ?, ?, ? ); ', array($time, $time, $cartId[0]->id , $options[$key]->option_id, $options[$key]->$price)) ;            
+                                            VALUES(?, ?, ?, ?, ? ); ', array($time, $time, $cartId[0]->id , $options[$key]->option_id, $options[$key]->price)) ;            
                         DB::commit();
     
                     }
-                    return array('2');
+                  
                 }
                 catch (Exception $e) {
                     DB::rollBack();
                     return exceped($e);
                 }
-
+                return array('777');
             }else{
                 return false;
             }
