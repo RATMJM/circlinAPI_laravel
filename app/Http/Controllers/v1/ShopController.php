a3fd1560d82cd5bb6a3706adfbf02d23b36b9301
JMMMM3 <jm@circlin.co.kr>
2021-08-24 20:45:33 +0900

    shop/cart 수정

--- app/Http/Controllers/v1/ShopController.php
+++ app/Http/Controllers/v1/ShopController.php
@@ -439,21 +439,24 @@ class ShopController extends Controller
                 }
                 
                 try {
-             
-                    foreach ($options as $key => $value){  
-                        
-                        $option = DB::insert('INSERT into cart_options(created_at, updated_at, cart_id, product_option_id, price)
-                                            VALUES(?, ?, ?, ?, ? ); ', array($time, $time, $cartId[0]->id , $options[$key]->option_id, $options[$key]->price)) ;            
-                        DB::commit();
-    
-                    }
+                    return success([
+                        '$cartId' => $cartId     ]);
+                     
+                    // foreach ($options as $key => $value){  
+                    //     if($options[$key]->option_id != NULL){
+                    //         $option = DB::insert('INSERT into cart_options(created_at, updated_at, cart_id, product_option_id, price)
+                    //         VALUES(?, ?, ?, ?, ? ); ', array($time, $time, $cartId[0]->id , $options[$key]->option_id, $options[$key]->price)) ;            
+                    //          DB::commit();
+                    //     }
+                       
+                     
                   
                 }
                 catch (Exception $e) {
                     DB::rollBack();
                     return exceped($e);
                 }
-                return array('777');
+              
             }else{
                 return false;
             }
