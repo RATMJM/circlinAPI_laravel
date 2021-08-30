00fe47e8b006de9818091195f81d1cb87fd33cec
lastedchan <451575@naver.com>
2021-08-30 17:44:01 +0900

    product_keywords -> keywords

--- app/Models/Keyword.php
+++ app/Models/Keyword.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ProductKeyword extends Model
+class Keyword extends Model
 {
     use HasFactory;
 
--- database/migrations/2021_08_25_121625_create_keywords_table.php
+++ database/migrations/2021_08_25_121625_create_keywords_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductKeywordsTable extends Migration
+class CreateKeywordsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class CreateProductKeywordsTable extends Migration
      */
     public function up()
     {
-        Schema::create('product_keywords', function (Blueprint $table) {
+        Schema::create('keywords', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('type');
             $table->string('area_code');
             $table->string('keyword');
             $table->softDeletes();
