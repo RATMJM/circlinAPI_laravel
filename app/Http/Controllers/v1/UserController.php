7af07a40db1e99127614c1f9c71ea2bfa641d7af
lastedchan <451575@naver.com>
2021-08-31 23:51:55 +0900

    GET /mypage/{feed,mission} 카테고리 필터 배열로 수정

--- app/Http/Controllers/v1/UserController.php
+++ app/Http/Controllers/v1/UserController.php
@@ -578,7 +578,7 @@ class UserController extends Controller
             })
             ->when($category_id, function ($query, $category_id) {
                 $query->whereHas('missions', function ($query) use ($category_id) {
-                    $query->whereIn('mission_categories.id', Arr::wrap($category_id));
+                    $query->whereIn('missions.mission_category_id', Arr::wrap($category_id));
                 });
             })
             ->select([
