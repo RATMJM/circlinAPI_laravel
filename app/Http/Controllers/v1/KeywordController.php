f1130c776819f0fd721a0e012c7498d384ae1bf5
lastedchan <451575@naver.com>
2021-09-07 19:17:17 +0900

    keyword area_code null인 경우 항상 제공되도록

--- app/Http/Controllers/v1/KeywordController.php
+++ app/Http/Controllers/v1/KeywordController.php
@@ -13,7 +13,10 @@ class KeywordController extends Controller
         $area_code = substr($request->get('area_code'), 0, 5);
 
         $keywords = Keyword::where('type', $type)
-            ->where('area_code', $area_code)
+            ->where(function ($query) use ($area_code) {
+                $query->where('area_code', $area_code)
+                    ->orWhereNull('area_code');
+            })
             ->orderBy('id', 'desc')
             ->pluck('keyword');
 
