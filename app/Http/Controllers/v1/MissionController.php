36063eb2d1aee9fd04fd3b9ff0a4ecd1525659c1
JMMMM3 <jm@circlin.co.kr>
2021-09-09 22:25:52 +0900

    with '#' will be ignored, and an empty message aborts the commit.

--- app/Http/Controllers/v1/MissionController.php
+++ app/Http/Controllers/v1/MissionController.php
@@ -851,7 +851,7 @@ class MissionController extends Controller
          ifnull(sum(b.distance),0) total_distance, 
            ifnull(ROUND((sum(b.distance) / c.goal_distance) * 100 ,0),0) as progress,
             sum( CASE WHEN cast(c.goal_distance as unsigned ) <= cast(b.distance as unsigned) then  1 else 0 end ) as success_today,
-            ifnull((select count(id) from feed_missions where mission_id= ? ) ,0) cert_count            
+            ifnull((select count(id) from feed_missions where mission_id= ? and  ) ,0) cert_count            
            from feeds a 
            left join feed_missions b on a.id=b.feed_id
            left join mission_stats c on b.mission_id=c.mission_id and b.mission_stat_id=c.id  and b.mission_stat_id= ?   
