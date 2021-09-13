ca7ac480e2d090753313debb3c811fa9b8296356
lastedchan <451575@naver.com>
2021-09-13 20:21:25 +0900

    어드민 페이지 추가

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\DeleteUser;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,54 +21,60 @@ Route::get('/', function (Request $request) {
     return $request->ip();
 });
 
-Route::get('/user/profile/image', function () {
-    echo "<form method='post' action='/v1/user/profile/image' enctype='multipart/form-data'>
-    <input type='file' name='file' id='file'>
-    <button type='submit'>업로드</button>
-</form>";
-});
+Route::group(['prefix' => 'admin', 'middleware' => ['web', 'admin'], 'as' => 'admin.'], function () {
+    Route::get('/', function (Request $request) {
+        return $request->ip();
+    });
 
-Route::get('/feed', function () {
-    echo "<form method='post' action='/v1/feed' enctype='multipart/form-data'>
-    <textarea name='content' cols='30' rows='10'></textarea><br>
-    <input type='text' name='is_hidden' value='0'><br>
-    <input type='checkbox' name='missions[]' checked value='1'>
-    <input type='checkbox' name='missions[]' checked value='20'>
-    <input type='checkbox' name='missions[]' checked value='40'>
-    <br>
-    <input type='text' name='product_brand' value='나이키'>
-    <input type='text' name='product_title' value='나이키 에어맥스 GENOME'>
-    <input type='text' name='product_price' value='199000'>
-    <input type='text' name='product_url' value='https://google.com/search?q=나이키 에어맥스 GENOME'>
-    <br>
-    <input type='text' name='place_address' value='서울시 동대문구 전농로 21'>
-    <input type='text' name='place_title' value='우리집'>
-    <input type='text' name='place_description' value='집'>
-    <input type='text' name='place_image' value='https://cyld20182.speedgabia.com/Image/profile/64175/3bNXkQOqRxj8t25Zf3Y2yCuxpDgK4zniuwtTHRaB.png'>
-    <input type='text' name='place_url' value='https://google.com/search?q=우리집'>
-    <br>
-    <input type='file' name='files[]' multiple><br>
-    <button type='submit'>업로드</button>
-</form>";
-});
+    Route::get('/login', function () {
+        return view('admin.login');
+    })->name('login.index');
+    Route::post('/login', function (Request $request) {
+        $user = $request->only('email', 'password');
 
-Route::get('/mission', function () {
-    echo "<form method='post' action='/v1/mission' enctype='multipart/form-data'>
-    <input type='hidden' name='mission_category_id' value='12'>
-    <textarea name='title' cols='30' rows='10'></textarea><br>
-    <input type='text' name='product_brand' value='농심'>
-    <input type='text' name='product_title' value='신라면'>
-    <input type='text' name='product_price' value='850'>
-    <input type='text' name='product_url' value='https://naver.com/'>
-    <br>
-    <input type='text' name='place_address' value='서울시 동대문구 전농로 21'>
-    <input type='text' name='place_title' value='우리집'>
-    <input type='text' name='place_description' value='집'>
-    <input type='text' name='place_image' value='https://cyld20182.speedgabia.com/Image/profile/64175/3bNXkQOqRxj8t25Zf3Y2yCuxpDgK4zniuwtTHRaB.png'>
-    <input type='text' name='place_url' value='https://google.com/search?q=우리집'>
-    <br>
-    <input type='file' name='thumbnail'>
-    <input type='file' name='files[]' multiple><br>
-    <button type='submit'>업로드</button>
-</form>";
+        if (Auth::attempt($user, true)) {
+            return redirect()->route('admin.user');
+        } else {
+            echo "<script>alert('로그인에 실패했습니다.');</script>";
+        }
+    })->name('login');
+    Route::get('/logout', function () {
+        Auth::logout();
+        return redirect(request()->headers->get('referer'));
+    })->name('logout');
+
+    Route::get('/user', function (Request $request) {
+        $filter = $request->get('filter', 'day');
+
+        $date = [
+            'all' => User::withoutTrashed(),
+            'day' => User::where('created_at', '>=', date('Y-m-d')),
+            'week' => User::where('created_at', '>=', date('Y-m-d', time() - (86400 * date('w')))),
+            'month' => User::where('created_at', '>=', date('Y-m')),
+        ];
+        $users_count = [
+            'all' => $date['all']->count(),
+            'day' => $date['day']->count(),
+            'week' => $date['week']->count(),
+            'month' => $date['month']->count(),
+        ];
+        $deleted_old_users_count = DeleteUser::whereNull('users.id')
+            ->leftJoin('users', 'users.id', 'delete_users.user_id')
+            ->count();
+        $deleted_users_count = User::onlyTrashed()->count();
+
+        $users = $date[$filter]->select([
+            'users.*', 'area' => area(),
+            'following' => \App\Models\Follow::selectRaw("COUNT(1)")->whereColumn('target_id', 'users.id')
+        ])
+            ->orderBy('id', 'desc')
+            ->paginate(50);
+
+        return view('admin.user', [
+            'users_count' => $users_count,
+            'deleted_old_users_count' => $deleted_old_users_count,
+            'deleted_users_count' => $deleted_users_count,
+            'users' => $users,
+        ]);
+    })->name('user');
 });
