1167c2ebad8a5fe46b9d6bfa8938b7d69a266e8a
lastedchan <451575@naver.com>
2021-09-27 11:17:07 +0900

    푸시 예약했던 부분 삭제

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -40,39 +40,6 @@ class Kernel extends ConsoleKernel
         $schedule->call([ScheduleController::class, 'mission_expire_warning_am'])->dailyAt('08:00')->name('1차 미션 인증 알림');
         $schedule->call([ScheduleController::class, 'mission_expire_warning_pm'])->dailyAt('20:00')->name('2차 미션 인증 알림');
 
-        $schedule->call(function () {
-            $msg = "[❍△❒] 이번 게임은 보물찾기 입니다. 
-'힌트1' 장소에서 12시부터 단 10분간 오렌지마스크를 지급할 예정입니다.🦑";
-            $users = MissionStat::where('mission_id', 1671)->pluck('user_id')->toArray();
-            PushController::gcm_notify($users, '써클인', $msg, '', 'mission',1671);
-            $users = MissionStat::where('mission_id', 1675)->pluck('user_id')->toArray();
-            PushController::gcm_notify($users, '써클인', $msg, '', 'mission',1675);
-        })->cron('0 11 25 9 *');
-        $schedule->call(function () {
-            $msg = "[❍△❒] 이번 게임은 보물찾기 입니다. 
-'힌트1' 장소에서 14시부터 단 10분간 오렌지마스크를 지급할 예정입니다.🦑";
-            $users = MissionStat::where('mission_id', 1672)->pluck('user_id')->toArray();
-            PushController::gcm_notify($users, '써클인', $msg, '', 'mission',1672);
-            $users = MissionStat::where('mission_id', 1676)->pluck('user_id')->toArray();
-            PushController::gcm_notify($users, '써클인', $msg, '', 'mission',1676);
-        })->cron('0 13 25 9 *');
-        $schedule->call(function () {
-            $msg = "[❍△❒] 이번 게임은 보물찾기 입니다. 
-'힌트1' 장소에서 16시부터 단 10분간 오렌지마스크를 지급할 예정입니다.🦑";
-            $users = MissionStat::where('mission_id', 1673)->pluck('user_id')->toArray();
-            PushController::gcm_notify($users, '써클인', $msg, '', 'mission',1673);
-            $users = MissionStat::where('mission_id', 1677)->pluck('user_id')->toArray();
-            PushController::gcm_notify($users, '써클인', $msg, '', 'mission',1677);
-        })->cron('0 15 25 9 *');
-        $schedule->call(function () {
-            $msg = "[❍△❒] 이번 게임은 보물찾기 입니다. 
-'힌트1' 장소에서 18시부터 단 10분간 오렌지마스크를 지급할 예정입니다.🦑";
-            $users = MissionStat::where('mission_id', 1674)->pluck('user_id')->toArray();
-            PushController::gcm_notify($users, '써클인', $msg, '', 'mission',1674);
-            $users = MissionStat::where('mission_id', 1678)->pluck('user_id')->toArray();
-            PushController::gcm_notify($users, '써클인', $msg, '', 'mission',1678);
-        })->cron('0 17 25 9 *');
-
         $schedule->command('telescope:prune --hours=72')->daily();
     }
 
