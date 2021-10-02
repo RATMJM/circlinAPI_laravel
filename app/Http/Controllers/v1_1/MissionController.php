ca16b9d6f57d4d5ef8fff417d06a8d1813f57954
lastedchan <lastedchan@gmail.com>
2021-10-02 10:35:39 +0900

    start_event_mission 개선

--- app/Http/Controllers/v1_1/MissionController.php
+++ app/Http/Controllers/v1_1/MissionController.php
@@ -1106,48 +1106,29 @@ class MissionController extends Controller
     {
         $user_id = token()->uid;
         $mission_id = $request->get('mission_id');
-        // $user_id         = 4;//token()->uid;
-        // $mission_id      = 786;//$request->get('mission_id');
-        $time = date("Y-m-d H:i:s");
-        // insertExtUserCode($uid,$code,$challId);
 
-
-        //미션마스터 조회
-        $mission_info = DB::select('select started_at, ended_at, CASE when date_add(SYSDATE() , interval + 9 hour ) between a.reserve_started_at and a.reserve_ended_at then "PRE"
-            when date_add(SYSDATE() , interval + 9 hour ) between a.started_at and a.ended_at then "START"
-            ELSE "" end as START_DATE_CHECK, 
-            (select   count(id) +1 from mission_stats where mission_id = ?) as user_count
-            From missions a
-            where id= ?;'
-            , [$mission_id, $mission_id]);
-
-        if ($mission_info[0]->START_DATE_CHECK == 'PRE') { // 오늘날짜와 비교해서 당일이면 상태를 시작상태(Y)로 변경
-            $state = 'R'; // 안쓰는 로직으로 변경됨. 무조건 다음날 부터 선택가능
-            $timestamp1 = $mission_info[0]->started_at;
-            $today = "";
-        } elseif ($mission_info[0]->START_DATE_CHECK == 'START') {
-            $state = 'Y';
-            // $timestamp1 = $checkDate;
-            $today = date("Y-m-d H:i:s");
-        } elseif ($mission_info[0]->START_DATE_CHECK == 'END') {
+        if (is_null($mission_id)) {
             return success([
-                'success' => 'END',
+                'result' => false,
+                'reason' => 'not enough data',
             ]);
         }
 
+        if (MissionStat::where(['user_id' => $user_id, 'mission_id' => $mission_id])->exists()) {
+            return success(['result' => false, 'reason' => 'already bookmark']);
+        } elseif (Mission::select(DB::raw("(missions.reserve_started_at is null or missions.reserve_started_at<='".date('Y-m-d H:i:s')."') and
+            (missions.reserve_ended_at is null or missions.reserve_ended_at>'".date('Y-m-d H:i:s')."') or
+            (missions.started_at is null or missions.started_at<='".date('Y-m-d H:i:s')."') and
+            (missions.ended_at is null or missions.ended_at>'".date('Y-m-d H:i:s')."') as is_available"))
+            ->where('id', $mission_id)->value('is_available')) {
+            $data = MissionStat::create([
+                'user_id' => $user_id,
+                'mission_id' => $mission_id,
+            ]);
 
-        try {
-            DB::beginTransaction();
-            //미션   입력
-            $start_mission = DB::insert('insert into mission_stats(created_at, updated_at, user_id, mission_id)
-                                            values( ?, ? ,? ,?) ;'
-                , [$time, $time,
-                    $user_id,
-                    $mission_id,
-                ]);
+            $user_count = MissionStat::where('mission_id', $mission_id)->count();
 
-            DB::commit();
-
+            // 조건별 푸시
             $pushes = MissionPush::where('mission_id', $mission_id)->get();
             if (count($pushes) > 0) {
                 foreach ($pushes as $push) {
@@ -1156,23 +1137,14 @@ class MissionController extends Controller
                     }
                 }
             }
-
-            $mission_stat_id = DB::select('select max(id) as mission_stat_id
-            From mission_stats a
-            where mission_id= ? and user_id= ?  ;'
-                , [$mission_id, $user_id]);
-
             return success([
                 'success' => true,
-                'mission_stat_id' => $mission_stat_id,
-                'user_count' => $mission_info[0]->user_count,
+                'mission_stat_id' => $data->id,
+                'user_count' => $user_count,
             ]);
-
-        } catch (Exception $e) {
-            DB::rollBack();
-            return exceped($e);
+        } else {
+            return success(['result' => false]);
         }
-
     }
 
     //참가자 조회
