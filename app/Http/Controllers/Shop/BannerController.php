cc40ac9e18173d9b68d7431cdc175df829462a29
lastedchan <451575@naver.com>
2021-10-07 14:32:33 +0900

    shop GET /banner 개선

--- app/Http/Controllers/Shop/BannerController.php
+++ app/Http/Controllers/Shop/BannerController.php
@@ -10,11 +10,11 @@ use Illuminate\Support\Facades\DB;
 
 class BannerController extends Controller
 {
-    public function index($type)
+    public function index()
     {
         $now = date('Y-m-d H:i:s');
 
-        $banners = Banner::whereIn('type', Arr::wrap($type))
+        $banners = Banner::where('type', 'shop')
             ->where(function ($query) use ($now) {
                 $query->where('started_at', '<=', $now)
                     ->orWhereNull('started_at');
@@ -23,15 +23,12 @@ class BannerController extends Controller
                 $query->where('ended_at', '>', $now)
                     ->orWhereNull('ended_at');
             })
-            ->leftJoin('common_codes', function ($query) {
-                $query->on('common_codes.ctg_sm', 'banners.link_type')
-                    ->where('common_codes.ctg_lg', 'click_action');
-            })
             ->select([
-                'banners.image', 'banners.link_type', 'common_codes.content_ko as link',
+                'banners.image', 'banners.link_type',
                 DB::raw("CASE WHEN link_type in ('mission','event_mission') THEN mission_id
                     WHEN link_type='product' THEN product_id
-                    WHEN link_type='notice' THEN notice_id END as link_id"), 'banners.link_url'
+                    WHEN link_type='notice' THEN notice_id END as link_id"),
+                'banners.link_url'
             ])
             ->orderBy('sort_num', 'desc')
             ->orderBy('banners.id', 'desc')
