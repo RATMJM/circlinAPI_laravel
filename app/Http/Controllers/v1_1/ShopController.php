c1c7a746066d369eaf02cbd8e972557d32a38283
lastedchan <451575@naver.com>
2021-10-14 17:12:50 +0900

    주문 결제 완료 시 상품별 결제금액에 옵션가 포함되지 않는 문제 수정

--- app/Http/Controllers/v1_1/ShopController.php
+++ app/Http/Controllers/v1_1/ShopController.php
@@ -782,7 +782,7 @@ class ShopController extends Controller
                 $brand_id = DB::select('select brand_id From products where id = ?;', [$value['product_id']]);
 
                 $product = DB::insert('INSERT into order_products(created_at, updated_at, order_id, price, product_id, qty)
-                                                    VALUES(?, ?, ?, ?, ?, ?); ', [$time, $time, $orderId[0]->id, $value['sale_price'], $value['product_id'], $value['qty']]);
+                                                    VALUES(?, ?, ?, ?, ?, ?); ', [$time, $time, $orderId[0]->id, $value['sale_price']+$value['opt_price1']+$value['opt_price2']+$value['opt_price3']+$value['opt_price4']+$value['opt_price5'], $value['product_id'], $value['qty']]);
 
                 if ($value['shipping_fee'] > 0) {
                     $shipping_fee = DB::insert('INSERT into order_products(created_at, updated_at, order_id, price, brand_id, qty)
