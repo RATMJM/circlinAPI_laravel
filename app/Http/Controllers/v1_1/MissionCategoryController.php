e0fbbc5411d3fae57d21ee20375c688d6bc86084
lastedchan <451575@naver.com>
2021-12-29 22:29:05 +0900

    이벤트미션 끝난 경우도 위에 보이도록

--- app/Http/Controllers/v1_1/MissionCategoryController.php
+++ app/Http/Controllers/v1_1/MissionCategoryController.php
@@ -137,8 +137,8 @@ class MissionCategoryController extends Controller
             })
             ->when($id == 0, function ($query) {
                 $query->where('is_event', 1)
-                    ->orderBy(DB::raw("#(missions.started_at is null or missions.started_at<=now()) and
-                    (missions.ended_at is null or missions.ended_at>now())"), 'desc')
+                    /*->orderBy(DB::raw("#(missions.started_at is null or missions.started_at<=now()) and
+                    (missions.ended_at is null or missions.ended_at>now())"), 'desc')*/
                     ->orderBy(DB::raw("`event_order` + IF(`event_order`>0, RAND() * 0.9, 0)"), 'desc')
                     ->orderBy(DB::raw("missions.id + IF(missions.id=1806, (RAND() * 2), 0)"), 'desc');
             })
