32f1f50707e01a643c26977ef1dd3339fd191353
lastedchan <451575@naver.com>
2022-01-04 21:10:28 +0900

    카테고리 정렬 수정

--- app/Http/Controllers/v1_1/MissionCategoryController.php
+++ app/Http/Controllers/v1_1/MissionCategoryController.php
@@ -52,7 +52,7 @@ class MissionCategoryController extends Controller
                 ->groupBy('mission_categories.id')
                 ->orWhere('mission_categories.id', 0)
                 ->orderBy(DB::raw("mission_categories.id=0")) // 이벤트 탭 맨 뒤로
-                ->orderBy(DB::raw("mission_categories.id=21")) // 기타 탭 맨 뒤로
+                ->orderBy(DB::raw("mission_categories.id=21"), 'desc') // 기타 탭 맨 앞으로
                 ->orderBy('bookmark_total', 'desc')
                 ->orderBy('is_favorite', 'desc')
                 ->orderBy('mission_categories.id', 'desc')
