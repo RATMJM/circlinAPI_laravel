1ae7cafd73b06a282da89acdb5f24b202a8a1ea5
lastedchan <451575@naver.com>
2022-03-25 19:44:00 +0900

    mission_stat_user_id 제거

--- app/Http/Controllers/v1_1/UserController.php
+++ app/Http/Controllers/v1_1/UserController.php
@@ -973,7 +973,7 @@ class UserController extends Controller
                 'missions.thumbnail_image', 'missions.success_count',
                 'mission_stat_id' => MissionStat::select('id')->whereColumn('mission_id', 'missions.id')
                     ->where('user_id', $uid)->limit(1),
-                'users.id as mission_stat_user_id',
+                // 'users.id as mission_stat_user_id',
                 'users.id as user_id', 'users.nickname', 'users.profile_image', 'users.gender',
                 'is_bookmark' => MissionStat::selectRaw('COUNT(1) > 0')->where('mission_stats.user_id', $uid)
                     ->whereColumn('mission_id', 'missions.id'),
