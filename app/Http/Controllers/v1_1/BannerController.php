19df243d7d31629cc2a953283c592b9a6327c1b6
lastedchan <451575@naver.com>
2022-03-29 13:21:20 +0900

    배너 보지않기 관련 처리

--- app/Http/Controllers/v1_1/BannerController.php
+++ app/Http/Controllers/v1_1/BannerController.php
@@ -27,38 +27,35 @@ class BannerController extends Controller
         $hid_at = User::where('id', $user_id)->value('banner_hid_at');
 
         if ((new Carbon($hid_at))->diff(now())->d >= 7) {
-            $hid_at = null;
-        }
-
-        $banners = Banner::whereIn('type', Arr::wrap($type))
-            ->when($hid_at, function ($query, $hid_at) {
-                $query->where('started_at', '>', $hid_at);
-            })
-            ->where(function ($query) use ($now) {
-                $query->where('started_at', '<=', $now)
-                    ->orWhereNull('started_at');
-            })
-            ->where(function ($query) use ($now) {
-                $query->where('ended_at', '>', $now)
-                    ->orWhereNull('ended_at');
-            })
-            ->leftJoin('common_codes', function ($query) {
-                $query->on('common_codes.ctg_sm', 'banners.link_type')
-                    ->where('common_codes.ctg_lg', 'click_action');
-            })
-            ->select([
-                'banners.id',
-                'banners.image',
-                'banners.link_type',
-                'common_codes.content_ko as link',
-                DB::raw("CASE WHEN link_type in ('mission','event_mission','event_mission_old') THEN mission_id
+            $banners = [];
+        } else {
+            $banners = Banner::whereIn('type', Arr::wrap($type))
+                ->where(function ($query) use ($now) {
+                    $query->where('started_at', '<=', $now)
+                        ->orWhereNull('started_at');
+                })
+                ->where(function ($query) use ($now) {
+                    $query->where('ended_at', '>', $now)
+                        ->orWhereNull('ended_at');
+                })
+                ->leftJoin('common_codes', function ($query) {
+                    $query->on('common_codes.ctg_sm', 'banners.link_type')
+                        ->where('common_codes.ctg_lg', 'click_action');
+                })
+                ->select([
+                    'banners.id',
+                    'banners.image',
+                    'banners.link_type',
+                    'common_codes.content_ko as link',
+                    DB::raw("CASE WHEN link_type in ('mission','event_mission','event_mission_old') THEN mission_id
                     WHEN link_type='product' THEN product_id
                     WHEN link_type='notice' THEN notice_id END as link_id"),
-                'banners.link_url',
-            ])
-            ->orderBy(DB::raw("`sort_num` + (RAND() * 0.9)"), 'desc')
-            ->orderBy('banners.id', 'desc')
-            ->get();
+                    'banners.link_url',
+                ])
+                ->orderBy(DB::raw("`sort_num` + (RAND() * 0.9)"), 'desc')
+                ->orderBy('banners.id', 'desc')
+                ->get();
+        }
 
         foreach ($banners as $i => $banner) {
             $params = match ($banner->link_type) {
