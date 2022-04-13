3211a3c552469de81518300cd3394ccd70d89b58
lastedchan <451575@naver.com>
2022-04-13 13:15:52 +0900

    v2 GET /follower /following 추가

--- routes/v2.php
+++ routes/v2.php
@@ -1,6 +1,8 @@
 <?php
 
 #region 유저 관련
+use App\Http\Controllers\v2\UserController;
+
 Route::group(['prefix' => 'user', 'as' => 'user.'], function () {
     #region 유저 상세 페이지
     Route::group(['prefix' => '{user_id}'], function () {
