81aabce800278f3d5cf7922023767262a592d980
lastedchan <451575@naver.com>
2022-05-30 11:14:59 +0900

    mission_notice 관련 테이블 추가

---
+++ app/Models/MissionNotice.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\MissionNotice
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|MissionNotice newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|MissionNotice newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|MissionNotice query()
+ * @mixin \Eloquent
+ */
+class MissionNotice extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/MissionNoticeImage.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\MissionNoticeImage
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|MissionNoticeImage newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|MissionNoticeImage newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|MissionNoticeImage query()
+ * @mixin \Eloquent
+ */
+class MissionNoticeImage extends Model
+{
+    use HasFactory;
+}
