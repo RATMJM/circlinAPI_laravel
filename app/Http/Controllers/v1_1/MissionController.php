61bc58fcf17ff2c27b1c61ca185616c7ff8766f1
circlindev-caleb <caleb@circlin.co.kr>
2022-07-04 11:17:08 +0900

    Added ongoing status condition to mission.

--- app/Http/Controllers/v1_1/MissionController.php
+++ app/Http/Controllers/v1_1/MissionController.php
@@ -1274,8 +1274,10 @@ class MissionController extends Controller
             'mission_grounds.logo_image',
             'mission_grounds.intro_video',
             DB::raw("CASE WHEN
-                    (missions.started_at is null or missions.started_at <= now()) and
-                    (missions.ended_at is null or missions.ended_at >= now())
+                    ((missions.started_at is null or missions.started_at <= now()) and
+                    (missions.ended_at is null or missions.ended_at >= now()))
+                    or
+                    ((missions.reserve_ended_at <= now()) and (missions.ended_at >= now()))
                 THEN 'ongoing'
                 WHEN (missions.reserve_started_at is null or missions.reserve_started_at <= now()) and
                     (missions.reserve_ended_at is null or missions.reserve_ended_at >= now())
