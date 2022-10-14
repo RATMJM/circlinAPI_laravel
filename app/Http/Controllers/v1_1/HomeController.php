4d6e37b968e7287979f750ef63af90ea70cce234
circlindev-caleb <caleb@circlin.co.kr>
2022-10-14 14:17:14 +0900

    newsfeed2 리턴 데이터 수정

--- app/Http/Controllers/v1_1/HomeController.php
+++ app/Http/Controllers/v1_1/HomeController.php
@@ -301,25 +301,179 @@ class HomeController extends Controller
         $page = $request->get('page', 0);
         $limit = $request->get('limit', 20);
 
-        $data = Feed::select('feeds.id')
-            ->join('follows', function ($query) use ($user_id) {
-                $query->on('follows.target_id', 'feeds.user_id')
-                    ->where('follows.user_id', $user_id);
-            })
+        $data = Follow::where('follows.user_id', $user_id)
+            // ->where(User::selectRaw("COUNT(1) > 0")->where('users.id', 'follows.user_id'), 1)
             ->where('feeds.is_hidden', false)
             ->where('feeds.created_at', '>=', date('Y-m-d H:i:s', time() - 86400))
-            ->orderBy(FeedLike::selectRaw("COUNT(1) > 0")->whereColumn('feed_id', 'feeds.id')
-                ->where('feed_likes.user_id', $user_id))
-            ->orderBy('id', 'desc');
+            ->join('users', function ($query) {
+                $query->on('users.id', 'follows.target_id')->whereNull('users.deleted_at');
+            })
+            ->join('feeds', function ($query) {
+                $query->on('feeds.user_id', 'users.id')->whereNull('feeds.deleted_at');
+            })
+            ->select('feeds.id')
+            ->orderBy('feeds.id', 'desc');
+        // $data = $data//->where(FeedLike::selectRaw("COUNT(1) > 0")->whereColumn('feed_id', 'feeds.id')->where('user_id', token()->uid), false)
+        //     ->skip($page * $limit)->take($limit);
+
+        $data = Feed::joinSub($data, 'f', function ($query) {
+            $query->on('f.id', 'feeds.id');
+        })
+            ->join('users', 'users.id', 'feeds.user_id')
+            ->leftJoin('feed_products', 'feed_products.feed_id', 'feeds.id')
+            ->leftJoin('products', 'products.id', 'feed_products.product_id')
+            ->leftJoin('brands', 'brands.id', 'products.brand_id')
+            ->leftJoin('outside_products', 'outside_products.id', 'feed_products.outside_product_id')
+            ->select([
+                'feeds.id',
+                'users.id as user_id',
+                'is_blocked' => Block::selectRaw('count(id)')->whereColumn('target_id', 'users.id')->where('user_id', $user_id),
+                'users.nickname',
+                'users.profile_image',
+                'users.gender',
+                'area' => area_like(),
+                'followers' => Follow::selectRaw("COUNT(1)")->whereColumn('target_id', 'users.id'),
+                'is_following' => Follow::selectRaw("COUNT(1) > 0")->whereColumn('target_id', 'users.id')
+                    ->where('user_id', $user_id),
+                'feeds.id as feed_id',
+                'feeds.created_at',
+                'feeds.content',
+                'has_images' => FeedImage::selectRaw("COUNT(1) > 1")->whereColumn('feed_id', 'feeds.id'), // 이미지 여러장인지
+                'image_type' => FeedImage::select('type')->whereColumn('feed_id', 'feeds.id')->orderBy('id')->limit(1),
+                'image' => FeedImage::select('image')->whereColumn('feed_id', 'feeds.id')->orderBy('id')->limit(1),
+                'feed_products.type as product_type',
+                'feed_products.product_id',
+                DB::raw("IF(feed_products.type='inside', brands.name_ko, outside_products.brand) as product_brand"),
+                DB::raw("IF(feed_products.type='inside', products.name_ko, outside_products.title) as product_title"),
+                DB::raw("IF(feed_products.type='inside', products.thumbnail_image, outside_products.image) as product_image"),
+                'outside_products.url as product_url',
+                DB::raw("IF(feed_products.type='inside', products.price, outside_products.price) as product_price"),
+                'place_address' => Place::select('address')->whereColumn('feed_places.feed_id', 'feeds.id')
+                    ->join('feed_places', 'feed_places.place_id', 'places.id')
+                    ->orderBy('feed_places.id')->limit(1),
+                'place_title' => Place::select('title')->whereColumn('feed_places.feed_id', 'feeds.id')
+                    ->join('feed_places', 'feed_places.place_id', 'places.id')
+                    ->orderBy('feed_places.id')->limit(1),
+                'place_description' => Place::select('description')->whereColumn('feed_places.feed_id', 'feeds.id')
+                    ->join('feed_places', 'feed_places.place_id', 'places.id')
+                    ->orderBy('feed_places.id')->limit(1),
+                'place_image' => Place::select('image')->whereColumn('feed_places.feed_id', 'feeds.id')
+                    ->join('feed_places', 'feed_places.place_id', 'places.id')
+                    ->orderBy('feed_places.id')->limit(1),
+                'place_url' => Place::select('url')->whereColumn('feed_places.feed_id', 'feeds.id')
+                    ->join('feed_places', 'feed_places.place_id', 'places.id')
+                    ->orderBy('feed_places.id')->limit(1),
+                'check_total' => FeedLike::selectRaw("COUNT(1)")->whereColumn('feed_id', 'feeds.id'),
+                'comment_total' => FeedComment::selectRaw("COUNT(1)")->whereColumn('feed_id', 'feeds.id'),
+                'emoji_total' => ChatUser::selectRaw("COUNT(distinct chat_messages.chat_room_id)")->withTrashed()
+                    ->whereColumn('chat_users.user_id', 'feeds.user_id')
+                    ->whereColumn('chat_messages.feed_id', 'feeds.id')
+                    ->join('chat_messages', function ($query) {
+                        $query->on('chat_messages.chat_room_id', 'chat_users.chat_room_id')
+                            ->whereColumn('chat_messages.user_id', '!=', 'chat_users.user_id');
+                    })->whereNotNull('message'),
+                'has_check' => FeedLike::selectRaw("COUNT(1) > 0")->whereColumn('feed_id', 'feeds.id')
+                    ->where('user_id', token()->uid), // 해당 피드에 체크를 남겼는가
+                'has_paid_check' => FeedLike::withTrashed()
+                    ->selectRaw("COUNT(1) > 0")
+                    ->whereColumn('feed_id', 'feeds.id')
+                    ->where('user_id', token()->uid), // 해당 피드에 체크를 남겼던적 있는가
+                'has_comment' => FeedComment::selectRaw("COUNT(1) > 0")->whereColumn('feed_id', 'feeds.id')
+                    ->where('user_id', token()->uid), // 해당 피드에 댓글을 남겼는가
+                'has_emoji' => ChatMessage::selectRaw("COUNT(1) > 0")->whereColumn('feed_id', 'feeds.id')
+                    ->whereNotNull('message') // 피드 공유와 반응 남김을 구분을 위해
+                    ->where('user_id', $user_id)->whereHas('room', function ($query) {
+                        $query->where('is_group', false)->whereHas('users', function ($query) {
+                            $query->whereColumn('user_id', 'feeds.user_id');
+                        });
+                    }), // 해당 피드로 이모지를 보낸 적이 있는가
+            ])
+            ->with([
+                'comments' => function ($query) {
+                    $query->select([
+                        "feed_comments.id",
+                        "feed_comments.feed_id",
+                        "feed_comments.group",
+                        "feed_comments.depth",
+                        DB::raw("feed_comments.deleted_at is not null as is_delete"),
+                        "feed_comments.created_at",
+                        "feed_comments.comment",
+                        'users.id as user_id',
+                        'users.nickname',
+                        'users.profile_image',
+                        'users.gender',
+                    ])
+                        ->join('users', 'users.id', 'user_id')
+                        ->orderBy('id', 'desc')->take(2);
+                },
+            ])
+            ->orderBy('has_check')
+            ->orderBy('feeds.id', 'desc');
         $feeds_count = $data->count();
         $data = $data->skip($page * $limit)->take($limit)
             ->get();
 
-        return success([
+        foreach ($data as $i => $item) {
+            $data[$i]->product = arr_group($data[$i], [
+                'type',
+                'id',
+                'brand',
+                'title',
+                'image',
+                'url',
+                'price',
+            ], 'product_');
+            $data[$i]->place = arr_group($data[$i], ['address', 'title', 'description', 'image', 'url'], 'place_');
+        }
+
+        $feed_id = $data->pluck('feed_id');
+
+        $missions = Mission::whereIn('feed_missions.feed_id', $feed_id)
+            ->join('feed_missions', 'feed_missions.mission_id', 'missions.id')
+            ->join('mission_categories', 'mission_categories.id', 'missions.mission_category_id')
+            ->select([
+                'feed_missions.feed_id',
+                'missions.id',
+                'missions.title',
+                'mission_categories.emoji',
+                'missions.is_event',
+                DB::raw("missions.id <= 1213 and missions.is_event = 1 as is_old_event"),
+                'missions.event_type',
+                'missions.is_ground',
+                'missions.is_ocr',
+                'missions.started_at',
+                'missions.ended_at',
+                'missions.thumbnail_image',
+                'missions.success_count',
+                'is_bookmark' => MissionStat::selectRaw('COUNT(1) > 0')->whereColumn('mission_id', 'missions.id')
+                    ->where('user_id', $user_id),
+                'mission_stat_id' => MissionStat::withTrashed()->select('id')->whereColumn('mission_id', 'missions.id')
+                    ->where('user_id', $user_id)->orderBy('id', 'desc')->limit(1),
+                'mission_stat_user_id' => MissionStat::withTrashed()
+                    ->select('user_id')
+                    ->whereColumn('mission_id', 'missions.id')
+                    ->where('user_id', $user_id)
+                    ->orderBy('id', 'desc')
+                    ->limit(1),
+                DB::raw("$user_id as mission_stat_user_id"),
+            ])
+            ->withCount([
+                'feeds' => function ($query) use ($user_id) {
+                    $query->where('user_id', $user_id);
+                },
+            ])
+            ->get();
+
+        foreach ($missions->groupBy('feed_id') as $i => $mission) {
+            $data[array_search($i, $feed_id->toArray())]->missions = $mission;
+        }
+
+        return [
             'result' => true,
             'totalCount' => $feeds_count,
             'data' => $data
-        ]);
+        ];
+
     }
 
     public function badge(): array
