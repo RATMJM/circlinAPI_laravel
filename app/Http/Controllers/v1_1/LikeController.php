614d56635965921604b1606e2f42360aee968fd0
circlindev-caleb <caleb@circlin.co.kr>
2022-10-27 19:37:39 +0900

    real_gathered_point 삭제

--- app/Http/Controllers/v1_1/LikeController.php
+++ app/Http/Controllers/v1_1/LikeController.php
@@ -116,9 +116,6 @@ class LikeController extends Controller
             $paid_point = false; // 대상에게 포인트 줬는지
             $take_point = false; // 10번 체크해서 포인트 받았는지
 
-            $real_gathered_point = $type === 'feed' ? 10 : 0; // 일일 수취 가능한도(500p)를 고려할 때 내가 실제로 얻은 포인트(feed_check_reward)
-            $real_gave_point = $type === 'feed' ? 10 : 0; // 일일 수취 가능한도(500p)를 고려할 때 상대가 실제로 얻은 포인트(feed_check)
-
             // 일 수취 가능 한도 1000P(100회 체크카운트) 제한을 해제한 코드
             if ($user_id == 61361 && $type === 'feed') {
                 $count = FeedLike::withTrashed()
@@ -146,34 +143,32 @@ class LikeController extends Controller
                     &&
                     Feed::where('id', $id)->first()->created_at >= init_today(time() - (86400))
                 ) {
-                    $res = PointController::change_point($target_id, $real_gave_point, 'feed_check', 'feed', $id);
+                    $res = PointController::change_point($target_id, $point += 10, 'feed_check', 'feed', $id);
                     $paid_point = $res['success'] && $res['data']['result'];
                     $real_gave_point = $paid_point ? $res['data']['point'] : 0;  //$res['point']
 
                     // 지금이 10번째 피드체크 && 오늘 하루 획득한 액수가 획득 가능한 포인트 상한선보다 낮을 경우만 지급
                     if ($count % 10 === 9 && $my_gatherable_point > 0) {
-                        $res = PointController::change_point($user_id, $real_gathered_point, 'feed_check_reward');
+                        $res = PointController::change_point($user_id, 10, 'feed_check_reward');
                         // 위에서 10p 지급을 요청했지만, 실제로는 획득 가능한 포인트 액수를 따르므로 그보다 적을수 있다.
-                        $real_gathered_point = $res['data']['result'] ? $res['data']['point'] : 0;
 
                         $daily_point_limit = (new PointController)->today_gatherable_point($user_id)['daily_limit'];
                         $current_gathered_point = (new PointController)->today_gatherable_point($user_id)['today_gathered_point'];
                         $gatherable_point = $daily_point_limit - $current_gathered_point;
 
                         NotificationController::send($user_id, 'feed_check_reward', null, null, false,
-                            ['point' => $real_gathered_point, 'point2' => $gatherable_point]); // ['point' => 10, 'point2' => 100 - ($count + 1)]);
+                            ['point' => $res['data']['point'], 'point2' => $gatherable_point]); // ['point' => 10, 'point2' => 100 - ($count + 1)]);
                         $take_point = $res['success'] && $res['data']['result'];
+
                     }
-                    $real_gathered_point = 0;
-                    $real_gave_point = 0;
                     $count += 1;
                 }
 
-                $data_like = $table_like->create(["{$type}_id" => $id, 'user_id' => $user_id, 'point' => $real_gave_point]);
+                $data_like = $table_like->create(["{$type}_id" => $id, 'user_id' => $user_id, 'point' => $point]);
 
                 $res = match ($type) {
                     'feed' => $paid_point ?
-                        NotificationController::send($data->user_id, 'feed_check', $user_id, $id, true, ['point' => $real_gave_point])
+                        NotificationController::send($data->user_id, 'feed_check', $user_id, $id, true, ['point' => $point])
                         : null,
                     // 'mission' => NotificationController::send($data->user_id, 'mission_like', $user_id, $id, true),
                     default => null,
@@ -185,7 +180,6 @@ class LikeController extends Controller
                     'paid_point' => $paid_point,
                     'result' => (bool)$data_like,
                     'take_point' => $take_point,
-                    'real_gathered_point' => $real_gathered_point,
                     'today_gathered_point' => $my_today_gathered_point
                 ]);
 
@@ -220,8 +214,6 @@ class LikeController extends Controller
                             ['point' => 10, 'point2' => 100 - ($count+1)]);
                         $take_point = $res['success'] && $res['data']['result'];
                     }
-                    $real_gathered_point = 0;
-                    $real_gave_point = 0;
                     $count += 1;
                 }
 
@@ -241,7 +233,6 @@ class LikeController extends Controller
                     'paid_point' => $paid_point,
                     'result' => (bool)$data_like,
                     'take_point' => $take_point,
-                    'real_gathered_point' => $real_gathered_point,
                     'today_gathered_point' => $my_today_gathered_point
                 ]);
             } else {}
